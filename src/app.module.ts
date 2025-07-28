19bfc1be0f6bb4d0144274c7194548d87466dbd6
emanuel <emanuelchusgp@gmail.com>
2025-07-28 14:35:57 -0400

    feat: agregar módulo de verificación de estado y actualizar configuraciones de Docker y Kubernetes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { RolesModule } from './modules/roles/roles.module';
 import { BoardsModule } from './modules/boards/boards.module';
 import { TasksModule } from './modules/tasks/tasks.module';
 import { WebSocketModule } from './modules/websockets/websocket.module';
+import { HealthCheckModule } from './modules/health-check/health-check.module';
  
 @Module({
-  imports: [UsersModule, RolesModule, BoardsModule, TasksModule,WebSocketModule],
+  imports: [UsersModule, RolesModule, BoardsModule, TasksModule,WebSocketModule, HealthCheckModule],
   controllers: [ ],
   providers: [ ],
 })
--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     }),
   );
   app.enableShutdownHooks();
-  console.log('Server Running on port: ', process.env.PORT ?? 3000);
+  console.log('Server Running on Port: ', process.env.PORT ?? 3000);
 
   await app.listen(process.env.PORT ?? 3000);
 }
